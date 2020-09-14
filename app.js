40ce926bcba11d9bcd5a15e74aec780fac279be5
Gustavo Camello <gucamello@gmail.com>
2020-09-14 18:13:55 +0100

    version before authentication

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const express = require('express'),
 // Basic setup
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 // Conect the db
 mongoose.connect('mongodb://localhost/sell_everything', {
