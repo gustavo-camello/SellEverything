92337647fcf072b06446f4cb0d5e437b05c8cebf
Gustavo Camello <gucamello@gmail.com>
2020-09-14 19:13:00 +0100

    Added authentication in the app

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const product = require('./modules/product');
 const comment = require('./modules/comment');
+const { authenticate } = require('passport');
 
 const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
       mongoose = require('mongoose'),
+      passport = require('passport'),
+      LocalStrategy = require('passport-local'),
       Product = require('./modules/product'),
       Comment = require('./modules/comment'),
+      User = require('./modules/user'),
       seedDB = require('./seeds');
 
 
@@ -25,6 +29,24 @@ mongoose.connect('mongodb://localhost/sell_everything', {
 .catch(error => console.log(error.message));
 
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: "This is a node application!",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // -------------------------------------------
 
@@ -85,7 +107,7 @@ app.get("/productsList/:id", (req, res) => {
 // COMMENTS ROUTE
 // ========================
 
-app.get('/productsList/:id/comments/new', (req, res) => {
+app.get('/productsList/:id/comments/new', isLoggedIn,  (req, res) => {
   Product.findById(req.params.id, (err, product) => {
     if (err) {
       console.log(err);
@@ -95,7 +117,7 @@ app.get('/productsList/:id/comments/new', (req, res) => {
   })
 });
 
-app.post('/productsList/:id/comments', (req, res) => {
+app.post('/productsList/:id/comments', isLoggedIn, (req, res) => {
   Product.findById(req.params.id, (err, product) => {
     if (err) {
       console.log(err);
@@ -113,6 +135,55 @@ app.post('/productsList/:id/comments', (req, res) => {
     }
   })
 });
+
+//  ======================
+// AUTHENTICATION ROUTES
+// ========================
+
+// Show the form to sign up
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// Handle the sign up logic
+app.post('/register', (req, res) => {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, ()=> {
+      res.redirect('/productsList');
+    })
+  })
+});
+
+// Show login form
+app.get('/login', (req, res) => {
+  res.render('login');
+}); 
+
+app.post('/login', passport.authenticate('local', 
+  {
+    successRedirect: '/productsList',
+    failureRedirect: '/login'
+  }), (req, res) => {
+});
+
+// Logout Route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/productsList');
+});
+
+// Middleware - check if user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
 
 // --------------------------------- SERVER LISTINING -------------------------------------
 app.listen(3000, '127.0.0.1', ()=> {
